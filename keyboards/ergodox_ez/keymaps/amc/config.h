d317100baffb4a2d6744d7f096ac1679dca98d6f
Alexander Courtis <acourtis@atlassian.com>
2020-03-14 14:52:35 +1100

    leave notes on experimentation around scan rate

--- keyboards/ergodox_ez/keymaps/amc/config.h
+++ keyboards/ergodox_ez/keymaps/amc/config.h
@@ -13,3 +13,4 @@
 
 // recommended by keyboards/ergodox_ez/readme.md with value 4 commonly used in community layouts
 #define QMK_KEYS_PER_SCAN 4
+// regardless of the above value, the ergodox scans ~800 per second (tested by instrumenting matrix_scan_user) which is far greater than the hhkb's at ~140. Dual function keys on the homen row are only usable with the much greater scan rate.
