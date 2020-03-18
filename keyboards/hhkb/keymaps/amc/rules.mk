796f942cd7a01180fcda6b9b536b440eb9593a15
Alexander Courtis <acourtis@atlassian.com>
2020-03-18 11:10:24 +1100

    hhkb: almost default layout; home row modifiers did not work

--- keyboards/hhkb/keymaps/amc/rules.mk
+++ keyboards/hhkb/keymaps/amc/rules.mk
@@ -1,13 +1,13 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE = yes
-COMMAND_ENABLE = no
+COMMAND_ENABLE = yes
 MOUSEKEY_ENABLE = no
 RGBLIGHT_ENABLE = no
 NKRO_ENABLE = yes
 
 # debug print statements will be preprocessor'd out unless yes
-CONSOLE_ENABLE = no
+CONSOLE_ENABLE = yes
 
 REL_PATH = keyboards/hhkb/keymaps/amc
 
