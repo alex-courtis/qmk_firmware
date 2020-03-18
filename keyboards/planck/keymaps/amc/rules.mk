1b2c997e75f31dd1c0410dcc3f4a7e08d00810ba
Alexander Courtis <acourtis@atlassian.com>
2020-03-18 16:15:34 +1100

    console / command is only for debug

--- keyboards/planck/keymaps/amc/rules.mk
+++ keyboards/planck/keymaps/amc/rules.mk
@@ -2,7 +2,10 @@ SRC += muse.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE = yes
-COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
 WEBUSB_ENABLE = yes
 ORYX_ENABLE = yes
+
+# don't have two devices enabled as hid_listen has no way of differentiating; this is for debug only
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
