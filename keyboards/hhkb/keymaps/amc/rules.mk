61e4fccf2045ccdf3c7452115bf70e2e1dbfc015
Alexander Courtis <acourtis@atlassian.com>
2020-03-18 16:15:34 +1100

    console / command is only for debug

--- keyboards/hhkb/keymaps/amc/rules.mk
+++ keyboards/hhkb/keymaps/amc/rules.mk
@@ -1,13 +1,13 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE = yes
-COMMAND_ENABLE = yes
 MOUSEKEY_ENABLE = no
 RGBLIGHT_ENABLE = no
 NKRO_ENABLE = yes
 
-# debug print statements will be preprocessor'd out unless yes
-CONSOLE_ENABLE = yes
+# don't have two devices enabled as hid_listen has no way of differentiating; this is for debug only
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
 
 REL_PATH = keyboards/hhkb/keymaps/amc
 
