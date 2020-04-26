dfc3bcd3f8ff083f6a1cd6ac8552be21931ae006
Alexander Courtis <acourtis@atlassian.com>
2020-04-26 14:02:35 +1000

    planck: inline the c generated from json

--- keyboards/planck/keymaps/amc/rules.mk
+++ keyboards/planck/keymaps/amc/rules.mk
@@ -9,10 +9,3 @@ COMMAND_ENABLE = no
 
 # don't have two devices enabled as hid_listen has no way of differentiating; this is for debug only
 CONSOLE_ENABLE = no
-
-REL_PATH = keyboards/planck/keymaps/amc
-
-SRC += $(KEYBOARD_OUTPUT)/src/keymap_gen.c
-
-$(KEYBOARD_OUTPUT)/src/keymap_gen.c: $(REL_PATH)/amc.json
-	bin/qmk json2c --output $(KEYBOARD_OUTPUT)/src/keymap_gen.c $(REL_PATH)/amc.json
