d4881c6e2955f8d3feded58f6d5f958d8a574c70
Alexander Courtis <acourtis@atlassian.com>
2020-04-26 17:26:37 +1000

    ergodox: use the qmk default debounce of 5 instead of the unusual ergodox_ez default of 30

--- keyboards/ergodox_ez/keymaps/amc/config.h
+++ keyboards/ergodox_ez/keymaps/amc/config.h
@@ -7,5 +7,9 @@
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 2
 
+// ergodox_ez default 30
+#undef DEBOUNCE
+#define DEBOUNCE 5
+
 // recommended by keyboards/ergodox_ez/readme.md with value 4 commonly used in community layouts
 #define QMK_KEYS_PER_SCAN 4
