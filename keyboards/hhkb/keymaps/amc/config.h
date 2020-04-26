2d3d6d65909546038ac2c6c28e5f602d68d1b6d2
Alexander Courtis <acourtis@atlassian.com>
2020-04-26 15:53:38 +1000

    hhkb: inline the c generated from json

--- keyboards/hhkb/keymaps/amc/config.h
+++ keyboards/hhkb/keymaps/amc/config.h
@@ -2,10 +2,3 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-
-#undef TAPPING_TERM
-// default 200 keyboards/hhkb/config.h
-#define TAPPING_TERM  150
-
-// dual function modifiers are sluggish with the default 1
-#define QMK_KEYS_PER_SCAN 4
--- keyboards/hhkb/keymaps/amc/rules.mk
+++ keyboards/hhkb/keymaps/amc/rules.mk
@@ -8,10 +8,3 @@ COMMAND_ENABLE = yes
 
 # don't have two devices enabled as hid_listen has no way of differentiating; this is for debug only
 CONSOLE_ENABLE = no
-
-REL_PATH = keyboards/hhkb/keymaps/amc
-
-SRC += $(KEYBOARD_OUTPUT)/src/keymap_gen.c
-
-$(KEYBOARD_OUTPUT)/src/keymap_gen.c: $(REL_PATH)/amc.json
-	bin/qmk json2c --output $(KEYBOARD_OUTPUT)/src/keymap_gen.c $(REL_PATH)/amc.json
