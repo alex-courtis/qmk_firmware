44a15774bc1fc551482c328056c7c32817f15c48
Alexander Courtis <acourtis@atlassian.com>
2020-04-26 15:23:00 +1000

    planck: remove unused codepaths

--- keyboards/planck/keymaps/amc/keymap.c
+++ keyboards/planck/keymaps/amc/keymap.c
@@ -3,41 +3,16 @@
 #include "muse.h"
 #endif
 #include "eeprom.h"
-#include "keymap_german.h"
-#include "keymap_nordic.h"
-#include "keymap_french.h"
-#include "keymap_spanish.h"
-#include "keymap_hungarian.h"
-#include "keymap_swedish.h"
-#include "keymap_br_abnt2.h"
-#include "keymap_canadian_multilingual.h"
-#include "keymap_german_ch.h"
-#include "keymap_jp.h"
-#include "keymap_bepo.h"
-
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
-#define KC_PC_UNDO LCTL(KC_Z)
-#define KC_PC_CUT LCTL(KC_X)
-#define KC_PC_COPY LCTL(KC_C)
-#define KC_PC_PASTE LCTL(KC_V)
-#define ES_LESS_MAC KC_GRAVE
-#define ES_GRTR_MAC LSFT(KC_GRAVE)
-#define ES_BSLS_MAC ALGR(KC_6)
-#define NO_PIPE_ALT KC_GRAVE
-#define NO_BSLS_ALT KC_EQUAL
 
 enum planck_keycodes {
-  RGB_SLD = EZ_SAFE_RANGE,
+    RGB_SLD = EZ_SAFE_RANGE,
 };
 
 enum planck_layers {
-  _BASE,
-  _LOWER,
-  _RAISE,
-  _ADJUST,
+    _BASE,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
 };
 
 #define LOWER MO(_LOWER)
@@ -71,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-  }
-  return true;
+    switch (keycode) {
+        case RGB_SLD:
+            if (record->event.pressed) {
+                rgblight_mode(1);
+            }
+            return false;
+    }
+    return true;
 }
 
 #ifdef AUDIO_ENABLE
@@ -109,13 +84,12 @@ void encoder_update(bool clockwise) {
             unregister_code(KC_PGDN);
         } else {
             register_code(KC_PGUP);
-            unregister_codE(KC_PGUP);
+            unregister_code(KC_PGUP);
         }
     }
 }
 
 void matrix_scan_user(void) {
-#ifdef AUDIO_ENABLE
     if (muse_mode) {
         if (muse_counter == 0) {
             uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
@@ -127,16 +101,15 @@ void matrix_scan_user(void) {
         }
         muse_counter = (muse_counter + 1) % muse_tempo;
     }
-#endif
 }
 
 bool music_mask_user(uint16_t keycode) {
     switch (keycode) {
-    case RAISE:
-    case LOWER:
-        return false;
-    default:
-        return true;
+        case RAISE:
+        case LOWER:
+            return false;
+        default:
+            return true;
     }
 }
 #endif
@@ -144,3 +117,4 @@ bool music_mask_user(uint16_t keycode) {
 uint32_t layer_state_set_user(uint32_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
