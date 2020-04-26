7a8c3926e085ebebca95cb1741c80ef6840c9145
Alexander Courtis <acourtis@atlassian.com>
2020-04-26 17:07:44 +1000

    tidy

--- keyboards/planck/keymaps/amc/rules.mk
+++ keyboards/planck/keymaps/amc/rules.mk
@@ -1,4 +1,3 @@
-SRC += muse.c
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE = yes
@@ -8,3 +7,4 @@ AUDIO_ENABLE = no
 
 # don't have two devices enabled as hid_listen has no way of differentiating; this is for debug only
 CONSOLE_ENABLE = no
+
