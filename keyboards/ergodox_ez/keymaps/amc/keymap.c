707cc47bdb51d5e671b21c8e630981f67e1eb2c4
Alexander Courtis <acourtis@atlassian.com>
2020-04-26 16:47:31 +1000

    ergodox: remove modifier squishing and use red led for layer 1

--- keyboards/ergodox_ez/keymaps/amc/keymap.c
+++ keyboards/ergodox_ez/keymaps/amc/keymap.c
@@ -7,29 +7,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 extern keymap_config_t keymap_config;
 
-static uint16_t num_lock_sets = 0;
-static uint16_t caps_lock_resets = 0;
-
-static uint8_t default_layer = 0;
-
-#ifdef MODIFIER_SIDE_SQUISHING
-static uint16_t left_squishes = 0;
-static uint16_t right_squishes = 0;
-
-#define LMODS (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_LCTRL) | MOD_BIT(KC_LALT) | MOD_BIT(KC_LGUI))
-#define L_ROW_LOW 1
-#define L_ROW_HIGH 5
-#define L_COL_LOW 0
-#define L_COL_HIGH 3
-
-#define RMODS (MOD_BIT(KC_RSHIFT) | MOD_BIT(KC_RCTRL) | MOD_BIT(KC_RALT) | MOD_BIT(KC_RGUI))
-#define R_ROW_LOW 8
-#define R_ROW_HIGH 12
-#define R_COL_LOW 0
-#define R_COL_HIGH 3
-
-#endif // MODIFIER_SIDE_SQUISHING
-
 void keyboard_post_init_user(void) {
 	ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
 	ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
@@ -44,32 +21,11 @@ void suspend_power_down_user(void) {
 	ergodox_right_led_3_off();
 }
 
-uint32_t default_layer_state_set_user(uint32_t state) {
-	default_layer = biton32(state);
-
-	dprintf("default_layer_state_set_user default_layer=%u\n", default_layer);
-
-	// default layer 1 blue
-	if (default_layer == 1) {
-		ergodox_right_led_3_on();
-	} else {
-		ergodox_right_led_3_off();
-	}
-
-	return state;
-}
-
 uint32_t layer_state_set_user(uint32_t state) {
 	uint8_t layer = biton32(state);
 
-	dprintf("layer_state_set_user layer=%u default_layer=%u\n", layer, default_layer);
-	dprintf("                     num_lock_sets=%u caps_lock_resets=%u\n", num_lock_sets, caps_lock_resets);
-#ifdef MODIFIER_SIDE_SQUISHING
-	dprintf("                     left_squishes=%u right_squishes=%u\n", left_squishes, right_squishes);
-#endif // MODIFIER_SIDE_SQUISHING
-
-	// fn layer 2 red
-	if (layer == 2) {
+	// red
+	if (layer == 1) {
 		ergodox_right_led_1_on();
 	} else {
 		ergodox_right_led_1_off();
@@ -77,43 +33,6 @@ uint32_t layer_state_set_user(uint32_t state) {
 
 	return state;
 };
-
-#ifdef MODIFIER_SIDE_SQUISHING
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	static uint8_t mods;
-
-	if (default_layer != 0) {
-		return true;
-	}
-
-	mods = get_mods();
-
-	// let things through when we have mods on both sides
-	// this is needed for things like command
-	// finer filtering might be possible, but probably not needed
-	if (mods & LMODS && mods & RMODS) {
-		return true;
-	}
-
-	// todo: let other modifiers through via LSFT_T and friends
-
-	if (mods & LMODS &&
-			record->event.key.col >= L_COL_LOW && record->event.key.col <= L_COL_HIGH &&
-			record->event.key.row >= L_ROW_LOW && record->event.key.row <= L_ROW_HIGH) {
-		left_squishes++;
-		return false;
-	}
-
-	if (mods & RMODS &&
-			record->event.key.col >= R_COL_LOW && record->event.key.col <= R_COL_HIGH &&
-			record->event.key.row >= R_ROW_LOW && record->event.key.row <= R_ROW_HIGH) {
-		right_squishes++;
-		return false;
-	}
-
-	return true;
-}
-#endif // MODIFIER_SIDE_SQUISHING
 
 void matrix_scan_user(void) {
 	static uint16_t lock_check_timer = 0;
@@ -125,14 +44,10 @@ void matrix_scan_user(void) {
 
 	if (timer_elapsed(lock_check_timer) > 500) {
 		led_state = host_keyboard_led_state();
-		if (!led_state.num_lock) {
+		if (!led_state.num_lock)
 			SEND_STRING(SS_TAP(X_NUMLOCK));
-			num_lock_sets++;
-		}
-		if (led_state.caps_lock) {
+		if (led_state.caps_lock)
 			SEND_STRING(SS_TAP(X_CAPSLOCK));
-			caps_lock_resets++;
-		}
 		lock_check_timer = timer_read();
 	}
 }
