54420ec82416e7fd3873842944f6a6c479bc13cb
Alexander Courtis <alex@courtis.org>
2020-06-02 12:13:06 +1000

    ergodox: update to match ka2

--- keyboards/ergodox_ez/keymaps/amc/keymap.c
+++ keyboards/ergodox_ez/keymaps/amc/keymap.c
@@ -2,14 +2,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ergodox_pretty(
-            KC_1,        KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_NO,       KC_NO,       KC_7,        KC_8,        KC_9,        KC_0,        KC_MINS,     KC_EQL,
-            KC_GRV,      KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_NO,       KC_NO,       KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_LBRC,
-            KC_BSLS,     KC_A,        KC_S,        KC_D,        KC_F,        KC_G,                                  KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_QUOT,
-            KC_NO,       KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        TT(1),       KC_NO,       KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,     KC_NO,
-            KC_NO,       KC_PSTE,     KC_LEFT,     KC_RIGHT,    KC_COPY,                                                         KC_RBRC,     KC_UP,       KC_DOWN,     LSFT(KC_INS),KC_NO,
-                                                                             KC_TAB,      KC_LALT,     KC_RALT,     KC_DEL,
-                                                                                          KC_NO,       KC_RSHIFT,
-                                                                KC_BSPC,     KC_ESC,      KC_NO,       KC_NO,       KC_ENT,      KC_SPC
+            KC_RBRC,     KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_NO,       KC_NO,       KC_7,        KC_8,        KC_9,        KC_0,        KC_MINS,     KC_EQL,
+            KC_1,        KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_NO,       KC_NO,       KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_QUOT,
+            KC_LBRC,     KC_A,        KC_S,        KC_D,        KC_F,        KC_G,                                  KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_BSLS,
+            KC_P0,       KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        TT(1),       KC_NO,       KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,     KC_GRV,
+            KC_NO,       KC_NO,       KC_LEFT,     KC_RIGHT,    KC_SPC,                                                          KC_BSPC,     KC_UP,       KC_DOWN,     LSFT(KC_INS),KC_NO,
+                                                                             KC_TAB,      KC_F6,       KC_F7,       KC_DEL,
+                                                                                          KC_F5,       KC_F8,
+                                                                KC_BSPC,     KC_ESC,      KC_F4,       KC_F9,       KC_ENT,      KC_SPC
              ),
     [1] = LAYOUT_ergodox_pretty(
             KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_PSCR,     KC_SLCK,     KC_PAUS,     KC_INS,      KC_NO,       KC_NLCK,     KC_PSLS,     KC_PAST,     KC_PMNS,     KC_NO,
