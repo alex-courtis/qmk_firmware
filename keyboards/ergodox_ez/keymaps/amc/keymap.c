dd8096cb633473d0a38aca42e7d3ce7bba0f69b0
Alexander Courtis <alex@courtis.org>
2020-11-18 17:07:33 +1100

    ergodox_ez: minimally bright LEDs and new layout

--- keyboards/ergodox_ez/keymaps/amc/keymap.c
+++ keyboards/ergodox_ez/keymaps/amc/keymap.c
@@ -1,34 +1,17 @@
 #include QMK_KEYBOARD_H
 
+// bin/qmk json2c ~/src/qmk_firmware/keyboards/ergodox_ez/keymaps/amc/amc.json
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_ergodox_pretty(
-            KC_RBRC,     KC_2,        KC_3,        KC_4,        KC_5,        KC_6,        KC_NO,               KC_NO,       KC_7,        KC_8,        KC_9,        KC_0,        KC_MINS,     KC_EQL,
-            KC_1,        KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,        KC_NO,               KC_NO,       KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,        KC_QUOT,
-            KC_LBRC,     KC_A,        KC_S,        KC_D,        KC_F,        KC_G,                             KC_H,        KC_J,        KC_K,        KC_L,        KC_SCLN,     KC_BSLS,
-            KC_P0,       KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,        TT(1),               KC_NO,       KC_N,        KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,     KC_GRV,
-            KC_NO,       KC_NO,       KC_LEFT,     KC_RIGHT,    KC_SPC,                                                     KC_BSPC,     KC_UP,       KC_DOWN,     LSFT(KC_INS),KC_NO,
-                                                                        LCTL_T(KC_TAB),   KC_F6,               KC_F7,           RCTL_T(KC_DEL),
-                                                                                          KC_F5,               KC_F8,
-                                                   LSFT_T(KC_BSPC),     LALT_T(KC_ESC),   KC_F4,               KC_F9,           RALT_T(KC_ENT),       RSFT_T(KC_SPC)
-             ),
-    [1] = LAYOUT_ergodox_pretty(
-            KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_PSCR,     KC_SLCK,     KC_PAUS,             KC_INS,      KC_NO,       KC_NLCK,     KC_PSLS,     KC_PAST,     KC_PMNS,     KC_NO,
-            KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,               KC_PGUP,     KC_NO,       KC_P7,       KC_P8,       KC_P9,       KC_PPLS,     KC_NO,
-            KC_CAPS,     KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,                            KC_HOME,     KC_P4,       KC_P5,       KC_P6,       KC_PPLS,     KC_NO,
-            KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_TRNS,             KC_PGDN,     KC_END,      KC_P1,       KC_P2,       KC_P3,       KC_PENT,     KC_NO,
-            KC_NO,       KC_NO,       KC_NO,       KC_NO,       KC_NO,                                                      KC_P0,       KC_P0,       KC_PDOT,     KC_PENT,     KC_NO,
-                                                                        KC_TRNS,          KC_NO,               KC_NO,           KC_TRNS,
-                                                                                          KC_NO,               KC_NO,
-                                                   KC_TRNS,             KC_TRNS,          KC_NO,               KC_NO,           KC_TRNS,              KC_TRNS
-            ),
+        [0] = LAYOUT_ergodox_pretty(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO, KC_NO, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_RBRC, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,
+KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_NO, KC_LSFT, KC_LEFT, KC_RGHT, KC_NO, KC_NO, KC_UP, KC_DOWN, LSFT(KC_INS), KC_NO, KC_TAB, KC_LGUI, KC_RGUI, KC_DEL, KC_F2, KC_F3, KC_BSPC, KC_ESC, KC_F1, KC_F4, KC_ENT, KC_SPC)
 };
 
 //extern keymap_config_t keymap_config;
 
 void keyboard_post_init_user(void) {
-    ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
-    ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
-    ergodox_right_led_3_set(LED_BRIGHTNESS_LO);
+    ergodox_right_led_1_set(LED_BRIGHTNESS_REALLY_LO);
+    ergodox_right_led_2_set(LED_BRIGHTNESS_REALLY_LO);
+    ergodox_right_led_3_set(LED_BRIGHTNESS_REALLY_LO);
 
     // very curious: this causes dfk to fail
     //keymap_config.nkro = true;
