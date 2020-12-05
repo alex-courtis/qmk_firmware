c58d24fd77003374a7553ceb2c99e3a8bc9025c0
Alexander Courtis <alex@courtis.org>
2020-12-06 10:35:40 +1100

    use mouse middle button instead of shift-ins

--- keyboards/planck/keymaps/amc/rules.mk
+++ keyboards/planck/keymaps/amc/rules.mk
@@ -1,7 +1,7 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LTO = yes
-MOUSEKEY_ENABLE = no
+MOUSEKEY_ENABLE = yes
 COMMAND_ENABLE = no
 AUDIO_ENABLE = no
 
