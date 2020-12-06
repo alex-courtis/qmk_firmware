3f24a0285394231d0610dedbfc3d1d77bdf3a198
Alexander Courtis <alex@courtis.org>
2020-12-06 13:46:56 +1100

    Use branch amc-planck-451e60047 until the upstream issue with planck lighting is resolved.

--- keyboards/planck/keymaps/amc/keymap.c
+++ keyboards/planck/keymaps/amc/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+Use branch amc-planck-451e60047 until the upstream issue with planck lighting is resolved.
+
 // bin/qmk json2c ~/src/qmk_firmware/keyboards/planck/keymaps/amc/amc.json
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT_planck_1x2uC(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_LCTL, KC_DEL, KC_LGUI, KC_LALT, MO(1), KC_SPC, MO(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
