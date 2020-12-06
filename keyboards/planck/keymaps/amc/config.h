a91b0efdea101fede8d338f59d390dfabdeb1875
Alexander Courtis <alex@courtis.org>
2020-12-06 13:44:17 +1100

    use the last known working qmk for planck

---
+++ keyboards/planck/keymaps/amc/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// needed to enable the front teeth and LED_LEVEL
+#define ORYX_CONFIGURATOR
+
+// layers in this repo are different to zsa
+#define PLANCK_EZ_LED_LOWER 1
+#define PLANCK_EZ_LED_RAISE 2
+#define PLANCK_EZ_LED_ADJUST 3
