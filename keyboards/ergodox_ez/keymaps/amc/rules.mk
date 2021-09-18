4cadf74a375fd74d38bf1d0d498f6de89cf0caf5
Alexander Courtis <alex@courtis.org>
2021-09-18 13:20:31 +1000

    ergodox_ez: disable mouse keys

--- keyboards/ergodox_ez/keymaps/amc/rules.mk
+++ keyboards/ergodox_ez/keymaps/amc/rules.mk
@@ -1,7 +1,7 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LTO_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
 RGBLIGHT_ENABLE = no
 COMMAND_ENABLE = no
 
