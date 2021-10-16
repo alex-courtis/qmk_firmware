f47faf52a91221a3fb3862389ce92205ce9b1a91
Alexander Courtis <alex@courtis.org>
2021-10-16 13:38:46 +1100

    all: orthodox consistency in enabling NKRO at startup

--- keyboards/planck/keymaps/amc/rules.mk
+++ keyboards/planck/keymaps/amc/rules.mk
@@ -1,9 +1,10 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LTO = yes
-MOUSEKEY_ENABLE = yes
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
 COMMAND_ENABLE = no
 AUDIO_ENABLE = no
+NKRO_ENABLE = yes
 
 # don't have two devices enabled as hid_listen has no way of differentiating; this is for debug only
 CONSOLE_ENABLE = no
