6060f016bfd809fa1d59b5b0b108c53f3083e01d
Alexander Courtis <alex@courtis.org>
2021-10-16 12:02:04 +1100

    dactyl: less force is necessary for nkro

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/amc/config.h
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/amc/config.h
@@ -24,6 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 
 #define MASTER_LEFT
-/* #define MASTER_RIGHT */
+// #define MASTER_RIGHT
 
 //#define EE_HANDS
--- keyboards/handwired/dactyl_manuform/6x6/keymaps/amc/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/amc/keymap.c
@@ -1,6 +1,2 @@
 #include "km.c"
 
-void keyboard_post_init_user(void) {
-    keymap_config.nkro = true;
-}
-
