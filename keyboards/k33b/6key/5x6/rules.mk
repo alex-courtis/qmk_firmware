d73f472333172d75ec540c3abc68d035c6781a02
Alexander Courtis <alex@courtis.org>
2023-09-24 13:53:02 +1000

    k33b: turn off unnecessary features

--- keyboards/k33b/6key/5x6/rules.mk
+++ keyboards/k33b/6key/5x6/rules.mk
@@ -6,9 +6,9 @@ BOOTLOADER = rp2040
 SERIAL_DRIVER = vendor
 
 # Build Options
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        	# Commands for debug and configuration
 NKRO_ENABLE = yes            # Enable N-Key Rollover
