824d777801f42d5e26a055ec9df275acaba5085c
Alexander Courtis <alex@courtis.org>
2024-01-09 13:29:14 +1100

    wylderbuilds: import from vial fork

---
+++ keyboards/handwired/wylderbuilds/trackball/4x6_whole_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCA, 0x97, 0x9B, 0x0F, 0xDF, 0xD6, 0x75, 0xD6}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
