eee458507b45f3919d0893dc1d080c30c020117c
Alexander Courtis <alex@courtis.org>
2024-03-04 18:04:30 +1100

    add gaming layer to tapdance

--- keyboards/rr/6key/5x6/config.h
+++ keyboards/rr/6key/5x6/config.h
@@ -23,3 +23,15 @@
 // diode direction placement
 #define DIODE_DIRECTION COL2ROW
 
+// TAP_MILLISEC: 200
+#define TAPPING_TERM 200
+
+// DOUBLE_TAP_MILLISEC: 150
+#define QUICK_TAP_TERM 150
+
+// consumption, mutex
+#define HOLD_ON_OTHER_KEY_PRESS
+// #define PERMISSIVE_HOLD
+
+// KEY on release without consumption
+// #define RETRO_TAPPING
