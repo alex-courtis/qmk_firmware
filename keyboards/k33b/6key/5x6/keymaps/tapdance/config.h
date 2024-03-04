bbb575ca3a9e13ea0e47ae9ea3d5698a318766bf
Alexander Courtis <alex@courtis.org>
2024-03-04 17:44:15 +1100

    k33b: tapdance layout

---
+++ keyboards/k33b/6key/5x6/keymaps/tapdance/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// Left half recognized by pin pulled high
+#define SPLIT_HAND_PIN GP27
+
+#undef PRODUCT
+#define PRODUCT "Dactyl Tap Dance 5x6"
+
+// TAP_MILLISEC: 200
+#define TAPPING_TERM 200
+
+// DOUBLE_TAP_MILLISEC: 150
+#define QUICK_TAP_TERM 150
+
+// consumption, mutex
+#define HOLD_ON_OTHER_KEY_PRESS
+// #define PERMISSIVE_HOLD
+
+// KEY on release without consumption
+// #define RETRO_TAPPING
+
