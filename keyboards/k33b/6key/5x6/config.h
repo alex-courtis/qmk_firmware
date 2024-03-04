9081fb350f12983744ce1b98dda9385b0866ad00
Alexander Courtis <alex@courtis.org>
2024-03-04 18:04:30 +1100

    add gaming layer to tapdance

--- keyboards/k33b/6key/5x6/config.h
+++ keyboards/k33b/6key/5x6/config.h
@@ -23,3 +23,20 @@
 
 // diode direction placement
 #define DIODE_DIRECTION ROW2COL
+
+// Left half recognized by pin pulled high
+#define SPLIT_HAND_PIN GP27
+
+// TAP_MILLISEC: 200
+#define TAPPING_TERM 200
+
+// DOUBLE_TAP_MILLISEC: 150
+#define QUICK_TAP_TERM 150
+
+// consumption, mutex
+#define HOLD_ON_OTHER_KEY_PRESS
+// #define PERMISSIVE_HOLD
+
+// KEY on release without consumption
+// #define RETRO_TAPPING
+
--- keyboards/k33b/6key/5x6/keymaps/default/config.h
+++ keyboards/k33b/6key/5x6/keymaps/default/config.h
@@ -1,7 +1,4 @@
 #pragma once
 
-// Left half recognized by pin pulled high
-#define SPLIT_HAND_PIN GP27
-
 #undef PRODUCT
 #define PRODUCT "Dactyl Manuform 5x6"
