6d40ea491aad797550b0ded1bcc7b943157cc386
Alexander Courtis <alex@courtis.org>
2024-03-11 17:00:20 +1100

    wylderbuilds: tapdance layout

--- keyboards/handwired/wylderbuilds/5x6/config.h
+++ keyboards/handwired/wylderbuilds/5x6/config.h
@@ -60,6 +60,18 @@
 #define LOCKING_SUPPORT_ENABLE
 // Locking resynchronize hack
 #define LOCKING_RESYNC_ENABLE
-// Enables This makes it easier for fast typists to use dual-function keys
-#define PERMISSIVE_HOLD
 // End of Basic Config
+
+
+// TAP_MILLISEC: 200
+#define TAPPING_TERM 200
+
+// DOUBLE_TAP_MILLISEC: 150
+#define QUICK_TAP_TERM 150
+
+// consumption, mutex
+#define HOLD_ON_OTHER_KEY_PRESS
+// #define PERMISSIVE_HOLD
+
+// KEY on release without consumption
+// #define RETRO_TAPPING
