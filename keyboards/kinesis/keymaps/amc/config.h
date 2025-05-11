8708e790c9efadb0512bf7d52a79e4ff8378b447
Alexander Courtis <alex@courtis.org>
2025-05-11 17:15:42 +1000

    kinesis: add kint41 and partial map; teensy cannot reset and takes 10sec to boot

---
+++ keyboards/kinesis/keymaps/amc/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define FORCE_NKRO
+
+// TAP_MILLISEC: 200
+#define TAPPING_TERM 200
+
+// DOUBLE_TAP_MILLISEC: 150
+#define QUICK_TAP_TERM 150
+
+// consume on down
+#define HOLD_ON_OTHER_KEY_PRESS
